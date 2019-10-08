168b4b8ec320f23358fafa853ce3db65cd1e6f99
Carlos Arrieta <carlosj.arrieta@gmail.com>
2019-10-08 02:59:32 -0500

    configura persos controller para que responda json

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -1,65 +1,42 @@
 class PersonasController < ApplicationController
-  before_action :set_persona, only: [:show, :edit, :update, :destroy]
+  before_action :set_persona, only: [:show, :update, :destroy]
   before_action :authenticate_user
 
   # GET /personas
-  # GET /personas.json
   def index
     @personas = Persona.all
+
+    render json: @personas
   end
 
   # GET /personas/1
-  # GET /personas/1.json
   def show
-  end
-
-  # GET /personas/new
-  def new
-    @persona = Persona.new
-  end
-
-  # GET /personas/1/edit
-  def edit
+    render json: @persona
   end
 
   # POST /personas
-  # POST /personas.json
   def create
     @persona = Persona.new(persona_params)
 
-    respond_to do |format|
-      if @persona.save
-        format.html { redirect_to @persona, notice: 'Persona was successfully created.' }
-        format.json { render :show, status: :created, location: @persona }
-      else
-        format.html { render :new }
-        format.json { render json: @persona.errors, status: :unprocessable_entity }
-      end
+    if @persona.save
+      render json: @persona, status: :created, location: @persona
+    else
+      render json: @persona.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /personas/1
-  # PATCH/PUT /personas/1.json
   def update
-    respond_to do |format|
-      if @persona.update(persona_params)
-        format.html { redirect_to @persona, notice: 'Persona was successfully updated.' }
-        format.json { render :show, status: :ok, location: @persona }
-      else
-        format.html { render :edit }
-        format.json { render json: @persona.errors, status: :unprocessable_entity }
-      end
+    if @persona.update(persona_params)
+      render json: @persona
+    else
+      render json: @persona.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /personas/1
-  # DELETE /personas/1.json
   def destroy
     @persona.destroy
-    respond_to do |format|
-      format.html { redirect_to personas_url, notice: 'Persona was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -68,7 +45,7 @@ class PersonasController < ApplicationController
       @persona = Persona.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def persona_params
       params.require(:persona).permit(:name, :surname, :nit, :phone, :area_id)
     end
